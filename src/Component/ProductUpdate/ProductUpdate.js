2fe312e92417d8bd3dfc29644d7db18909f17b34
saddamhosan <contact.saddamhosan.com>
2022-04-29 21:38:45 +0600

    added update section

--- src/Component/ProductUpdate/ProductUpdate.js
+++ src/Component/ProductUpdate/ProductUpdate.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const ProductUpdate = () => {
+  const navigate=useNavigate()
   const { id } = useParams();
   const [product, setProduct] = useState({});
   useEffect(() => {
@@ -13,6 +14,48 @@ const ProductUpdate = () => {
       setProduct(data);
     })();
   }, [id]);
+
+const handleQuantity=(e)=>{
+  e.preventDefault()
+  const quantity=e.target.quantity.value
+  const newQuantity = product?.quantity + parseInt(quantity);
+
+
+fetch(`https://enigmatic-beach-29740.herokuapp.com/product/${id}`, {
+  method: "PUT",
+  headers: {
+    "content-type": "application/json",
+  },
+  body: JSON.stringify({ newQuantity }),
+})
+  .then((res) => res.json())
+  .then((data) => {
+    if (data.modifiedCount > 0) {
+      console.log("Added Successfully ");
+      navigate("/");
+    }
+  });
+}
+
+const handleDelivered=()=>{
+const newQuantity = product?.quantity - 1
+
+fetch(`https://enigmatic-beach-29740.herokuapp.com/product/${id}`, {
+  method: "PUT",
+  headers: {
+    "content-type": "application/json",
+  },
+  body: JSON.stringify({ newQuantity }),
+})
+  .then((res) => res.json())
+  .then((data) => {
+    if (data.modifiedCount > 0) {
+      console.log("successfully delivered");
+      navigate("/");
+    }
+  });
+}
+
   const { img, name, supplier, price, quantity, description } = product;
   return (
     <div>
@@ -25,9 +68,13 @@ const ProductUpdate = () => {
         <p>Supplier: {supplier}</p>
         <p>Quantity: {quantity}</p>
         <p>$ {price}</p>
-        <button className="bg-blue-600 text-white font-bold px-5 py-2 mt-2 rounded-xl">
+        <button onClick={handleDelivered} className="bg-blue-600 text-white font-bold px-5 py-2 mt-2 rounded-xl">
           Delivered
         </button>
+        <form onSubmit={handleQuantity}>
+          <input type="number" name="quantity" placeholder="Add Quantity" required/>
+          <input type="submit" value="Add" />
+        </form>
       </div>
     </div>
   );
