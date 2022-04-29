326c98df6467dc409dbcab32c3a584bf24f32dfa
saddamhosan <contact.saddamhosan.com>
2022-04-29 12:44:39 +0600

    added slider

--- src/Component/Home/Banner.js
+++ src/Component/Home/Banner.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
+import banner1 from '../../images/banner1.jpg';
+import banner2 from '../../images/banner2.jpg';
+import banner3 from '../../images/banner3.jpg';
+import banner4 from '../../images/banner4.jpg';
+import banner5 from '../../images/banner5.jpg';
+import banner6 from '../../images/banner6.jpg';
 
 const Banner = () => {
     const settings = {
@@ -10,30 +16,30 @@ const Banner = () => {
       slidesToShow: 3,
       slidesToScroll: 1,
       autoplay: true,
-      speed: 2000,
+      speed: 3000,
       autoplaySpeed: 2000,
       cssEase: "linear"
     }
     return (
-         <div className='mb-6'>
+      <div className="mb-6">
         <Slider {...settings}>
           <div>
-            <h3>1</h3>
+            <img className='h-[300px]' src={banner1} alt="" />
           </div>
           <div>
-            <h3>2</h3>
+            <img className='h-[300px]' src={banner2} alt="" />
           </div>
           <div>
-            <h3>3</h3>
+            <img className='h-[300px]' src={banner3} alt="" />
           </div>
           <div>
-            <h3>4</h3>
+            <img className='h-[300px]' src={banner4} alt="" />
           </div>
           <div>
-            <h3>5</h3>
+            <img className='h-[300px]' src={banner5} alt="" />
           </div>
           <div>
-            <h3>6</h3>
+            <img className='h-[300px]' src={banner6} alt="" />
           </div>
         </Slider>
       </div>
